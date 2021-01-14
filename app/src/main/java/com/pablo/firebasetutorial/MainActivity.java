8593da87863a4ea85fd0c8b976cddee02df04f3e
Pablo-Jose <pablojmsb2014@gmail.com>
2021-01-14 20:12:33 -0300

    implementando o realtime database

--- app/src/main/java/com/pablo/firebasetutorial/MainActivity.java
+++ app/src/main/java/com/pablo/firebasetutorial/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference();
+
+        mDatabaseReference.push().child("nome").setValue("Pablo");
     }
 }
