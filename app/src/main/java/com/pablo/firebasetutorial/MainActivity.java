ecbaf8001ab499c8efc400d7d5c410916c3daa20
Thais <alvesthais406@gmail.com>
2021-01-15 09:41:55 -0300

    Java

--- app/src/main/java/com/pablo/firebasetutorial/MainActivity.java
+++ app/src/main/java/com/pablo/firebasetutorial/MainActivity.java
@@ -3,6 +3,11 @@ package com.pablo.firebasetutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
+    private TextView text, nome, input;
+    Button botaoOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,28 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference();
 
-        mDatabaseReference.push().child("nome").setValue("Pablo");
+        input = (AutoCompleteTextView) findViewById(R.id.input);
+        nome = (TextView) findViewById(R.id.nome);
+        text = (TextView) findViewById(R.id.text);
+        botaoOk = (Button) findViewById(R.id.botao_ok);
+
+        Onclik();
     }
+
+    private void Onclik() {
+        botaoOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(input.getText().length() <=0){
+                    input.setError("Digite um nome");
+                }else{
+                    String nomeUsuario = input.getText().toString().trim();
+                    nome.setText(nomeUsuario);
+                    nome.setVisibility(View.VISIBLE);
+                    mDatabaseReference.push().child("Nome").setValue(nomeUsuario);
+                }
+            }
+        });
+    }
+
 }
